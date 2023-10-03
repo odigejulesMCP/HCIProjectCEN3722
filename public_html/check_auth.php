5280e3ddbeb39b70a5d761faf4d6b7100a483387
poetryInMotion365 <ojules30@outlook.com>
2023-10-02 21:51:46 -0400

    The redirect path has been corrected to go to the login page in case the user is not authenticated.

--- public_html/check_auth.php
+++ public_html/check_auth.php
@@ -4,7 +4,7 @@ session_start();
 // Check if user is not authenticated
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     // Redirect to the login page
-    header("https://odigejulesweatherapp.com/index.php");
+    header('Location: /index.php');
     exit;
 }
 ?>
